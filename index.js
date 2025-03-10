51e01a3205149d4e048319c45a98ec037e743733
Sharon kosgei <jebetsharon089@gmail.com>
2025-03-10 21:03:47 +0300

    complete lab Arrow function

---
+++ index.js
@@ -0,0 +1,11 @@
+// Function Expression for divide
+const divide = function(num1, num2) {
+    return num1 / num2;
+  };
+  
+  // Arrow Function for square
+  const square = num => num * num;
+  
+  // Arrow Function for add
+  const add = (num1, num2) => num1 + num2;
+  
